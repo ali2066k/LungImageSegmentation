36c0e997e74f666e67e3a118cc08c604ef5e8fd0
ali2066k <74789127+ali2066k@users.noreply.github.com>
2021-10-21 03:19:30 +0200

    Update Util.py

--- Util.py
+++ Util.py
@@ -271,3 +271,40 @@ def multi_class_preparation(X, Y, test_size=0.2, n_classes=3):
 	valX_images = np.expand_dims(valX, axis=3)
 
 	return train_images, valX_images, trainY, valY
+
+
+# https://www.kaggle.com/cpmpml/fast-iou-metric-in-numpy-and-tensorflow
+# https://www.kaggle.com/meaninglesslives/nested-unet-with-efficientnet-encoder
+def get_iou_vector(A, B):
+    # Numpy version    
+    batch_size = A.shape[0]
+    metric = 0.0
+    for batch in range(batch_size):
+        t, p = A[batch], B[batch]
+        true = np.sum(t)
+        pred = np.sum(p)
+        
+        # deal with empty mask first
+        if true == 0:
+            metric += (pred == 0)
+            continue
+        
+        # non empty mask case.  Union is never empty 
+        # hence it is safe to divide by its number of pixels
+        intersection = np.sum(t * p)
+        union = true + pred - intersection
+        iou = intersection / union
+        
+        # iou metrric is a stepwise approximation of the real iou over 0.5
+        iou = np.floor(max(0, (iou - 0.45)*20)) / 10
+        
+        metric += iou
+        
+    # teake the average over all images in batch
+    metric /= batch_size
+    return metric
+
+
+def my_iou_metric(label, pred):
+    # Tensorflow version
+    return tf.py_func(get_iou_vector, [label, pred > 0.5], tf.float64)
